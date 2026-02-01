f4c237f7c9683f52c3304baf6fabf5a539b2df0d
averogirg <nikolaevnason@gmail.com>
2026-02-01 20:47:57 +0300

    Add files via upload

---
+++ sql/my_sql_queries.sql
@@ -0,0 +1,35 @@
+-- Изучаю анализ данных на примере продаж
+-- Посмотреть первые 5 строк таблицы
+SELECT * FROM superstore 
+LIMIT 5;
+
+-- Найти все покупки дороже 500 долларов
+SELECT customer_name, product_name, sales
+FROM superstore
+WHERE sales > 500;
+
+-- Показать товары от самых дорогих к самым дешёвым
+SELECT product_name, sales FROM superstore
+ORDER BY sales DESC;
+
+-- Посчитать, сколько всего продаж в каждой категории
+SELECT category,
+    COUNT(*) as number_of_products,  -- количество товаров
+    SUM(sales) as total_sales        -- общая сумма продаж
+FROM superstore
+GROUP BY category
+ORDER BY total_sales DESC;
+
+-- Найти всех клиентов из Лос-Анджелеса
+SELECT customer_name, city, product_name FROM superstore
+WHERE city LIKE '%Los Angeles%';
+
+-- Кто наши топ-3 клиента по сумме покупок?
+SELECT 
+    customer_name,
+    SUM(sales) as total_spent,
+    COUNT(*) as orders_count
+FROM superstore
+GROUP BY customer_name
+ORDER BY total_spent DESC
+LIMIT 3;
